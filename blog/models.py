444ef4088020dfeeeed8e3699887a422a02b9c9b
Ahmed <ahmedissaten@gmail.com>
2024-10-21 01:56:08 -0700

    adding status class and feild

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,21 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True) # will change when creating an object
     updated = models.DateTimeField(auto_now=True) # will change when saving an object
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
     class Meta:
         ordering = ['-publish']
