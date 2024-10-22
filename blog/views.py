7887a6eb374abc6a68a71da8ffd7640fca329954
Ahmed <ahmedissaten@gmail.com>
2024-10-22 00:58:55 -0700

    add post list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
+from django.http import Http404
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(
+        request,
+        'blog/post/posts.html',
+        {'posts': posts}
+    )
