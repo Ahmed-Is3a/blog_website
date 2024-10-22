6c127ef86f4daa9b327af655e2f2d09c49a3c3f0
Ahmed <ahmedissaten@gmail.com>
2024-10-22 01:04:41 -0700

    add post detail view

--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,14 @@ def post_list(request):
         'blog/post/posts.html',
         {'posts': posts}
     )
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No post found')
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
