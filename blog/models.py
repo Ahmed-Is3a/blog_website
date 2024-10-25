a7b46d72d43e99083f94efec37a2f5d6265160b2
Ahmed <ahmedissaten@gmail.com>
2024-10-25 14:19:54 -0700

    update get_absulate_url method by repalcing id by date and slug

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -16,7 +17,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True) # will change when creating an object
@@ -44,5 +45,9 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
-    
+        return reverse("blog:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
