566ce32ab00f77ee9ca28d1923b4dc74bf7986b2
Ahmed <ahmedissaten@gmail.com>
2024-10-27 04:04:31 -0700

    Handle post_sharing via email

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from blog.forms import EmailPostForm
 from blog.models import Post
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -46,4 +48,39 @@ def post_detail(request, year, month, day, post):
         request,
         'post/detail.html',
         {'post': post}
+    )
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,id=post_id, status=Post.Status.PUBLISHED)
+    send = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f"recommends you to read {post.title}"
+            )
+            message = (
+                f"read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comment']}"
+            )
+
+            send_mail(subject=subject,message=message,from_email=None, recipient_list=[cd['to']] )
+            send = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'send': send
+        }
     )
