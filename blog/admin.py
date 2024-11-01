45cc846d804ab408d8c1e50299b1d3627423d92d
Ahmed <ahmedissaten@gmail.com>
2024-11-01 13:11:40 -0700

    register comment model into admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Comment, Post
 
 # Register your models here.
 
 admin.site.register(Post)
+# admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post']
+    list_filter = ['active']
+    search_fields = ['name']
+    
+
